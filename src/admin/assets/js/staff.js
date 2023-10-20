113209f6f7c42e7d191e71fb90c2e6c70369df54
Furkan Alkaya <falkaya.dev@gmail.com>
2023-10-20 20:43:20 +0300

    last update

--- src/admin/assets/js/staff.js
+++ src/admin/assets/js/staff.js
@@ -1,49 +1,34 @@
 const employeeForm = document.getElementById("employee-form");
 console.log(employeeForm);
-const cardContainer = document.getElementById("card-container");
-console.log(cardContainer);
+const staffList = document.getElementById("staff-list");
+console.log(staffList);
 const createBtn = document.getElementById("create-btn");
 
 createBtn.addEventListener("click", function () {
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
   const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const today = new Date();
+  const day = today.getDate();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const dateFormat = year + "-" + month + "-" + day;
 
   // Create a new employee card
-  const card = document.createElement("div");
-  card.classList.add(
-    "w-full",
-    "h-full",
-    "bg-yellow-400",
-    "flex",
-    "items-center",
-    "px-8",
-    "rounded-xl",
-    "gap-4"
-  );
-
-  const img = document.createElement("img");
-  img.classList.add(
-    "w-16",
-    "h-16",
-    "rounded-full",
-    "border-2",
-    "border-primary-3"
-  );
-  img.src = "https://i.pravatar.cc/300";
-  img.alt = "Employee Photo";
-
-  const info = document.createElement("div");
-  info.classList.add("flex", "flex-col", "justify-center");
-  info.innerHTML = `
-        <span class="text-2xl">${firstName} ${lastName}</span>
-        <span class="text-sm">${email}</span>
-    `;
-
-  cardContainer.appendChild(card);
-  card.appendChild(img);
-  card.appendChild(info);
-
+  const row = staffList.insertRow(staffList.rows.length);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  cell1.innerHTML = `${firstName} ${lastName}`;
+  cell1.classList.add("text-center", "border-b", "text-sm");
+  cell2.innerHTML = `${email}`;
+  cell2.classList.add("text-center", "border-b", "text-sm");
+  cell3.innerHTML = `${password}`;
+  cell3.classList.add("text-center", "border-b", "text-sm");
+  cell4.innerHTML = ` ${dateFormat}`;
+  cell4.classList.add("text-center", "border-b", "text-sm");
   // Clear form inputs
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
