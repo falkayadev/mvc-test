3d544f565df337d816b19d42607395fb25cdbe69
Furkan Alkaya <falkaya.dev@gmail.com>
2023-10-24 12:39:46 +0300

    adminler de aktif pasif

--- src/admin/assets/js/profile.js
+++ src/admin/assets/js/profile.js
@@ -6,3 +6,25 @@ editButton.addEventListener("click", function () {
   select.classList.toggle("appearance-none");
   select.classList.toggle("bg-primary-6");
 });
+
+const profileStatus = document.getElementById("profileStatus");
+function profileFunc() {
+  if (profileStatus.getAttribute("data-status") === "active") {
+  profileStatus.classList.remove("bg-green-400");
+  profileStatus.classList.add("bg-gray-400");
+  profileStatus.innerHTML = "Passive";
+  profileStatus.setAttribute("data-status", "passive")
+  } else {
+    profileStatus.classList.remove("bg-gray-400", "bg-red-400")
+    profileStatus.classList.add("bg-green-400")
+    profileStatus.innerHTML = "Active";
+    profileStatus.setAttribute("data-status", "active")
+  }
+}
+
+function banFunc() {
+  profileStatus.classList.remove("bg-green-400", "bg-gray-400")
+  profileStatus.classList.add("bg-red-400")
+  profileStatus.innerHTML = "Banned"
+  profileStatus.setAttribute("data-status", "banned")
+}
