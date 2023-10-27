71b99dd406b65ed253903bdf632156bc177134db
Furkan Alkaya <falkaya.dev@gmail.com>
2023-10-27 22:25:04 +0300

    support

--- src/admin/assets/js/admin-ticket.js
+++ src/admin/assets/js/admin-ticket.js
@@ -1,18 +1,17 @@
 $(document).ready( function () {
     $('#myTable').DataTable({
         paging:true,
-        responsive: {
-            breakpoints: [
-              {name: 'bigdesktop', width: Infinity},
-              {name: 'meddesktop', width: 1480},
-              {name: 'smalldesktop', width: 1280},
-              {name: 'medium', width: 1188},
-              {name: 'tabletl', width: 1024},
-              {name: 'btwtabllandp', width: 848},
-              {name: 'tabletp', width: 768},
-              {name: 'mobilel', width: 480},
-              {name: 'mobilep', width: 320}
-            ]
-          }
+        responsive:true,
     });
 } );
+
+const triggers = document.querySelectorAll('.trigger-chat');
+const chatbox = document.querySelector('#supChatBox');
+
+triggers.forEach(function(trigger) {
+
+    trigger.addEventListener('click', function() {
+        chatbox.classList.toggle('hidden');
+    });
+
+});
