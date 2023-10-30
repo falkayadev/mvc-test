50b4f19d9b9976991a030cd664da81cca69d6d28
Furkan Alkaya <falkaya.dev@gmail.com>
2023-10-30 13:49:34 +0300

    fees and limits deposit açık geliyor

--- src/admin/assets/js/fees-and-limits.js
+++ src/admin/assets/js/fees-and-limits.js
@@ -1,20 +1,18 @@
-const triggers = document.querySelectorAll('.section-trigger');
+const triggers = document.querySelectorAll(".section-trigger");
 const sections = document.querySelectorAll('[id^="section"]');
 console.log(triggers);
 
-triggers.forEach(function(trigger) {
-   trigger.addEventListener('click', function (e) {
-
+triggers.forEach(function (trigger) {
+  trigger.addEventListener("click", function (e) {
     const targetId = this.getAttribute("data-target");
     const targetSection = document.getElementById(targetId);
 
-    sections.forEach(function(section) {
-        if (section !== targetSection) {
-            section.classList.add('hidden');
-        } else {
-            section.classList.toggle('hidden');
-        }
+    sections.forEach(function (section) {
+      if (section !== targetSection) {
+        section.classList.add("hidden");
+      } else {
+        section.classList.remove("hidden");
+      }
     });
-
-   }) 
+  });
 });
